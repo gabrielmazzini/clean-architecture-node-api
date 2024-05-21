683fb074ba61b974e157ff49e45708e6bc2a97d8
Gabriel Mazzini <100140182+gabrielmazzini@users.noreply.github.com>
2024-05-21 11:58:20 -0300

    implementado deletar usuario, mais melhorias nas estruturas de pastas

--- functions/src/useCases/CreateUser/index.ts
+++ functions/src/useCases/CreateUser/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import {MailTrapMailerProvider} from "../../dataSource/mail-service/MailTrapMailerProvider";
-import {DataBaseUserRepository} from "../../dataSource/repositories/databaseUserRepository/DataBaseUserRepository";
+import {CreateUserDatasource} from "./datasource/createUserDatasource";
 import {AdminFirebase} from "../../dataSource/firebase/repository/Admin-firebase";
 import {FirebaseService} from "../../dataSource/firebase/service/firebase-service";
 import {CreateUserController} from "./controller/CreateUserController";
@@ -9,11 +9,11 @@ import {CreateUserUseCase} from "./useCase/CreateUserUseCase";
 const firebaseService = new FirebaseService();
 const firebaseInstance = new AdminFirebase(firebaseService);
 
-const postgressUsersRepository = new DataBaseUserRepository(firebaseInstance);
+const createUserDatasource = new CreateUserDatasource(firebaseInstance);
 const mailTrapMailProvider = new MailTrapMailerProvider();
 
 const createUserUseCase = new CreateUserUseCase(
-  postgressUsersRepository,
+  createUserDatasource,
   mailTrapMailProvider,
 );
 
